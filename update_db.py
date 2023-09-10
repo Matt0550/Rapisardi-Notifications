8886fa14815a32279b3b42de3e1c4dc9e06d4c3f
Matteo Sillitti <m.sillitti@tde-dev.tech>
2023-09-10 20:57:35 +0200

    mongodb fix

--- update_db.py
+++ update_db.py
@@ -24,13 +24,9 @@ if MONGODB_USERNAME == None or MONGODB_USERNAME == "" or MONGODB_PASSWORD == Non
     exit()
 
 # Connect to the database
-client = MongoClient(MONGODB_HOST, MONGODB_PORT, username=MONGODB_USERNAME, password=MONGODB_PASSWORD, authSource=MONGODB_DATABASE)
+client = MongoClient("mongodb+srv://" + MONGODB_USERNAME+ ":" + MONGODB_PASSWORD +"@" + MONGODB_HOST)
 db = client[MONGODB_DATABASE]
 usersDb = db.users
-
-# Print all the users
-for user in usersDb.find():
-    print(user)
 
 # Setup SMTP server
 SMTP_SERVER = os.getenv("SMTP_HOST")
@@ -38,6 +34,8 @@ SMTP_PORT = int(os.getenv("SMTP_PORT"))
 SMTP_USER = os.getenv("SMTP_USERNAME")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 465))
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_SSL = os.getenv("SMTP_SSL", True)
+SMTP_FROM = os.getenv("SMTP_FROM")
 
 # Check if the SMTP password is set
 if SMTP_PASSWORD == None or SMTP_PASSWORD == "":
@@ -48,7 +46,7 @@ def sendEmail(to, subject, body, html=False):
     # Edit the mittent
 
     msg = MIMEMultipart()
-    msg["From"] = "Rapisardi Updates <" + SMTP_USER + ">"
+    msg["From"] = "Rapisardi Notifications <" + SMTP_FROM + ">"
     msg["To"] = to
     msg["Subject"] = subject
 
@@ -57,18 +55,19 @@ def sendEmail(to, subject, body, html=False):
     else:
         msg.attach(MIMEText(body, "plain"))
     text = msg.as_string()
-    server = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+    if SMTP_SSL == True or SMTP_SSL == "True":
+        server = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+    else:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
     server.login(SMTP_USER, SMTP_PASSWORD)
-    server.sendmail(SMTP_USER, to, text)
+    server.sendmail(SMTP_FROM, to, text)
     server.quit()
 
 def getUserDataFromDB(userId):
     # Get the user data from the database
     userData = usersDb.find_one({"id": userId})
     return userData
-
     
-
 def checkUpdates():
     try:
         sostituzioni = Sostituzioni("https://www.rapisardidavinci.edu.it/sost/app/sostituzioni.php")
@@ -78,43 +77,47 @@ def checkUpdates():
         else:
             updates = sostituzioni.getNextUpdates()
         
-    
         for update in updates:
             print("Checking update for " + update["classe"])
             # Search users where the class is the same as the update 
-            userClass = usersDb.find({"classe": update["classe"]})
-            for user in userClass:
-                print(user)
+            usersInClass = usersDb.find({"classe": update["classe"]})      
+            for user in usersInClass:
 
-            if len(list(userClass)) == 0:
-                print("No users found for " + update["classe"])
-                continue
-            
-            for user in userClass:
-                print("Checking " + update["classe"] + " for " + userClass)
+                # if len(list(usersInClass)) == 0:
+                #     print("No users found for " + update["classe"] + " in the database")
+                #     continue
 
-                if update["classe"].lower() != userClass.lower():
+                print("Checking " + update["classe"] + " for " + user["email"])
+
+                if update["classe"].lower() != user["classe"].lower():
                     continue
 
-                print("Found update for " + userClass)
+                print("Found update for " + user["email"])
                 # Make a table with the data
                 table = "<table><tr><th>Ora</th><th>Sostituzione</th></tr>"
                 for i in range(len(update["ore"])):
                     table += "<tr><td>" + update["ore"][i] + "</td><td>" + update["sostituzioni"][i] + "</td></tr>"
                 table += "</table>"
+                table += "<br> Docenti assenti: " + update["docentiAssenti"]
                 # Send the mail
-                sendEmailToUser(user, user["email"], table, update["sostituzioni"], update["date"], userClass)
+                sendEmailToUser(user, table, update["sostituzioni"], update["date"])
+
+        requests.get("https://hc-ping.com/822bf142-8aa4-4621-b3e7-f517ac2bf28f", timeout=10)
+
         return True
             
     except Exception as e:
         print("Error: unable to fetch data")
         print(e)
+        print(e.__traceback__.tb_lineno)
         return None
     
-def sendEmailToUser(userDBData, email, table, array, date, userClass):
-    print(array)
-    lastSent = userDBData[2]
-    lastContent = userDBData[3]
+def sendEmailToUser(userDBData, table, array, date):
+    lastSent = userDBData["last_notification"] if "last_notification" in userDBData else datetime.now()
+    lastContent = userDBData["last_sostituzioni"] if "last_sostituzioni" in userDBData else ""
+
+    email = userDBData["email"]
+    userClass = userDBData["classe"]
 
     # Check if the email has already been sent but if the content is different send it again
     if lastSent.strftime("%Y-%m-%d") == datetime.now().strftime("%Y-%m-%d") and lastContent == str(array):
@@ -122,27 +125,17 @@ def sendEmailToUser(userDBData, email, table, array, date, userClass):
         return None
     else:
         # Send the email
-        message = "Ciao, le sostituzioni in data " + date + " della classe " + userClass + " sono state aggiornate: <br> <br>" + table
+        message = "Ciao, le sostituzioni in data " + date + " della classe " + userClass + " (Sede Viale R. Margherita) sono state aggiornate: <br> <br>" + table
         sendEmail(email, "Aggiornamento sostituzioni", message, True)
 
         # Update the last sent date and content
-        sql = 'UPDATE users SET last_notification = "' + str(datetime.now()) + '", last_sostituzioni = "' + str(array) + '" WHERE id = ' + str(userDBData[0]) + ';'
-        try:
-            cursor.execute(sql)
-            db.commit()
-            print("Updated user " + str(userDBData[0]))
-            return True
-        except Exception as e:
-            print("Error: unable to update user")
-            print(e)
-            return None
+        usersDb.update_one({"_id": userDBData["_id"]}, {"$set": {"last_notification": datetime.now(), "last_sostituzioni": str(array)}})
+        print("Email sent to " + email)
+        return True
+
+
 
 def main():
-    try:
-        requests.get("https://hc-ping.com/822bf142-8aa4-4621-b3e7-f517ac2bf28f", timeout=10)
-    except requests.RequestException as e:
-        # Log ping failure here...
-        print("Ping failed: %s" % e)
     checkUpdates()
 
 if __name__ == "__main__":
